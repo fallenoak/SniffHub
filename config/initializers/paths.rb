91a2f735a43523ae9d893038c56f23a01e8d749d
fallenoak <fallenoak@uh-oh.co>
2015-11-23 18:35:01 -0600

    Updated paths config to match new directory structure.

--- config/initializers/paths.rb
+++ config/initializers/paths.rb
@@ -1,3 +1,3 @@
-UPLOADS_DIR = "#{Rails.root}/uploads"
-CAPTURES_DIR = "#{Rails.root}/captures"
-WORK_DIR = "#{Rails.root}/work"
+UPLOADS_DIR = "#{Rails.root}/data/uploads"
+CAPTURES_DIR = "#{Rails.root}/data/captures"
+WORK_DIR = "#{Rails.root}/data/work"
