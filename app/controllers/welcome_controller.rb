8c4b835be8a0542f79337a15d1b4e785f7242561
fallenoak <fallenoak@uh-oh.co>
2015-11-30 19:24:26 -0600

    Improved welcome/home styling.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,20 @@
 class WelcomeController < ApplicationController
+  skip_before_action :require_user, only: :index
+
   def index
     page[:primary_nav] = :home
+
+    if current_user?
+      load_home
+    else
+      load_welcome
+    end
+  end
+
+  private def load_home
+    @recent_uploads = current_user.uploads.order('created_at DESC').limit(5)
+  end
+
+  private def load_welcome
   end
 end
